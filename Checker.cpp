c8114181d64e36f566d7087a8031a4d9a836caed
Shathya47 <nettemshathya47@gmail.com>
2025-07-24 09:19:43 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,38 +1,67 @@
+#include <assert.h>
+
 #include <iostream>
 
-bool is_within_limits(float temperature, float soc, float cnn) {
-    return (temperature >= 0 && temperature <= 45) &&
-           (soc >= 20 && soc <= 80) &&
-           (cnn <= 0.8) &&
-           (cnn <= 3);  // Added cnn <= 3 condition
-}
+using namespace std;
+ 
+bool isTemperatureOk(float temperature) {
 
-bool battery_is_ok(float temperature, float soc, float cnn) {
-    if (is_within_limits(temperature, soc, cnn)) {
-        return true;
-    }
-    std::cout << "Temperature, State of Charge and Charge Rate is out of range!" << std::endl;
+  if(temperature < 0 || temperature > 45) {
+
+    cout << "Temperature out of range!\n";
+
     return false;
-}
 
+  }
+
+  return true;
+
+}
+ 
+bool isSocOk(float soc) {
+
+  if(soc < 20 || soc > 80) {
+
+    cout << "State of Charge out of range!\n";
+
+    return false;
+
+  }
+
+  return true;
+
+}
+ 
+bool isChargeRateOk(float chargeRate) {
+
+  if(chargeRate > 0.8) {
+
+    cout << "Charge Rate out of range!\n";
+
+    return false;
+
+  }
+
+  return true;
+
+}
+ 
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+
+  return isTemperatureOk(temperature) &&
+
+         isSocOk(soc) &&
+
+         isChargeRateOk(chargeRate);
+
+}
+ 
 int main() {
-    if (battery_is_ok(25, 70, 0.7)) {
-        std::cout << "Battery is OK for (25, 70, 0.7)" << std::endl;
-    } else {
-        std::cout << "Battery check failed for (25, 70, 0.7)" << std::endl;
-    }
 
-    if (!battery_is_ok(50, 85, 0)) {
-        std::cout << "Battery check failed as expected for (50, 85, 0)" << std::endl;
-    } else {
-        std::cout << "Battery is OK for (50, 85, 0), which is unexpected" << std::endl;
-    }
+  assert(batteryIsOk(25, 70, 0.7) == true);
 
-    if (!battery_is_ok(30, 50, 3.5)) {
-        std::cout << "Battery check failed as expected for cnn > 3 (3.5)" << std::endl;
-    } else {
-        std::cout << "Battery is OK for cnn > 3, which is unexpected" << std::endl;
-    }
+  assert(batteryIsOk(50, 85, 0) == false);
 
-    return 0;
 }
+
+ 
